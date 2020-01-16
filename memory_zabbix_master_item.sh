aacab25aa099227a780f3ed27d8ae26526df6b42
khinpyaephyosan <19262583+khinpyaephyosan@users.noreply.github.com>
2020-01-16 12:32:59 +0630

    Update memory_zabbix_master_item.sh
    ![image](https://user-images.githubusercontent.com/19262583/72496805-1859bb00-3859-11ea-8362-2f33ecb1f7ba.png)
    
    according to free command on centos 7, I changed the value to assign Buff and Cached variables. according to this link (https://sites.google.com/a/thetnaing.com/therunningone/how-to-calculate-systems-memory-utilization), changed the value to assign memory usage total.
    
    MEM_TOTAL=Total-(Free+Buff) buff and cache are the same value so can use Buff or Cached variable.
    
    Sincerely,

--- memory_zabbix_master_item.sh
+++ memory_zabbix_master_item.sh
@@ -2,21 +2,22 @@
 
 # Get current memory usage
 if [ -f "/etc/redhat-release" ]; then
-        MemUsage=`free | awk 'NR==2 {printf("%d %d %d %d",$2,$4,$6,$7)}'`
+        MemUsage=`free | awk 'NR==2 {printf("%d %d %d %d",$2,$4,$6,$6)}'`
 elif [ -f "/etc/lsb-release" ]; then
-        MemUsage=`free -w | awk 'NR==2 {printf("%d %d %d %d",$2,$4,$6,$7)}'`
+        MemUsage=`free -w | awk 'NR==2 {printf("%d %d %d %d",$2,$4,$6,$6)}'`
 else
         echo "Not supported distribution."
 fi
 
 # Retrieve each element
+#buff and cache value are the same 
 Total=`echo $MemUsage | awk '{print $1}'`
 Free=`echo $MemUsage | awk '{print $2}'`
 Buff=`echo $MemUsage | awk '{print $3}'`
 Cached=`echo $MemUsage | awk '{print $4}'`
 
 # Create JSON string
-echo "$MemUsage $(( Total -  Free - Buff - Cached))" | \
+echo "$MemUsage $(( Total -  (Free + Buff)))" | \
 awk \
 '{printf("{\
 \"MEM_TOTAL\":%d ,\
@@ -25,4 +26,5 @@ awk \
 \"MEM_CACHED\":%d ,\
 \"MEM_USED\":%d }",\
 $1,$2,$3,$4,$5)}'
+
 
